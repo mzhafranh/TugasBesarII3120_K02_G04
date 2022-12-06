3335d67ada652c4f9dac8a2e6d66fd1c11afb218
mzhafranh <16520139@std.stei.itb.ac.id>
2022-12-06 18:43:12 +0700

    renewed database

--- lasti.sql
+++ lasti.sql
@@ -21,6 +21,21 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
+-- Name: parking; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.parking (
+    id_parking character varying(10) NOT NULL,
+    location character varying(150) NOT NULL,
+    current_capacity integer NOT NULL,
+    total_capacity integer NOT NULL,
+    price integer NOT NULL
+);
+
+
+ALTER TABLE public.parking OWNER TO postgres;
+
+--
 -- Name: users; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -35,6 +50,19 @@ CREATE TABLE public.users (
 ALTER TABLE public.users OWNER TO postgres;
 
 --
+-- Data for Name: parking; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.parking (id_parking, location, current_capacity, total_capacity, price) FROM stdin;
+P01	Parkiran MTC	50	70	3000
+P02	Parkiran SMAN 5 Bandung	17	20	2000
+P03	Parkiran SMAN 3 Bandung	20	30	2000
+P04	Parkiran Centrum Bandung	10	50	2000
+P05	Parkiran ITB	230	300	2000
+\.
+
+
+--
 -- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
@@ -42,6 +70,22 @@ COPY public.users (email, username, password, role) FROM stdin;
 admin@gmail.com	admin	$2b$10$TYJs..XStVWFMGPUPApWrO/Z9RP2MAov4hmQknUpmFEqApozy.BYm	admin
 operator@gmail.com	operator	$2b$10$lJft0LhwjFaarACTq1XcM.OXdvQXnbvAYYRmm8ROxm8.QS3G38cb.	op
 \.
+
+
+--
+-- Name: parking parking_location_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.parking
+    ADD CONSTRAINT parking_location_key UNIQUE (location);
+
+
+--
+-- Name: parking parking_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.parking
+    ADD CONSTRAINT parking_pkey PRIMARY KEY (id_parking);
 
 
 --
